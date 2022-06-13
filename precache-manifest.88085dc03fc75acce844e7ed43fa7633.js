3a32e665a9a84c94ea4de107809a08a6e2eb71e9
Yukesh Sitoula <ys2ck@virginia.edu>
2022-06-12 21:47:21 -0400

    Updates

---
+++ precache-manifest.88085dc03fc75acce844e7ed43fa7633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f553221e2c85d7589c2c",
+    "url": "/robofriends/static/css/main.e20d78b1.chunk.css"
+  },
+  {
+    "revision": "f553221e2c85d7589c2c",
+    "url": "/robofriends/static/js/main.f553221e.chunk.js"
+  },
+  {
+    "revision": "6baae3a4a0e3c2342152",
+    "url": "/robofriends/static/css/1.cbf47567.chunk.css"
+  },
+  {
+    "revision": "6baae3a4a0e3c2342152",
+    "url": "/robofriends/static/js/1.6baae3a4.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "85202261772738e526cf98e5d2bcff27",
+    "url": "/robofriends/index.html"
+  }
+];
