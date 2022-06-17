32400bb3e43c13bdeb6b1c070fa5b8eaf414bba4
Yukesh Sitoula <ys2ck@virginia.edu>
2022-06-16 21:47:24 -0400

    Updates

---
+++ precache-manifest.d7ee4aa4aa809a0f38aae923c9130512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b042acb208cc0a44053",
+    "url": "/robofriends/static/css/main.e132f524.chunk.css"
+  },
+  {
+    "revision": "6b042acb208cc0a44053",
+    "url": "/robofriends/static/js/main.6b042acb.chunk.js"
+  },
+  {
+    "revision": "be1620c2c5c888c26629",
+    "url": "/robofriends/static/css/1.213bccc2.chunk.css"
+  },
+  {
+    "revision": "be1620c2c5c888c26629",
+    "url": "/robofriends/static/js/1.be1620c2.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdd4e0a12047364fdacafa357aeb3cbc",
+    "url": "/robofriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.fc0853e356ffc6518f99832c5efd3118.js"
+  "/robofriends/precache-manifest.d7ee4aa4aa809a0f38aae923c9130512.js"
 );
 
 workbox.clientsClaim();
